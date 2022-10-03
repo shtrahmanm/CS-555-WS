c51cc408d182d7f026a6c647227f9f412cc08704
bponna <55261231+bponna@users.noreply.github.com>
2022-10-02 21:16:08 -0400

    Add files via upload

--- gedcom_data.py
+++ gedcom_data.py
@@ -1,13 +1,17 @@
 from ast import Div
+from asyncore import write
 from distutils.debug import DEBUG
 import sys
 from pathlib import Path
 from telnetlib import SE
+import datetime
 from prettytable import PrettyTable
 Individuals = PrettyTable()
 Families = PrettyTable()
 valid_tags={"INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL",
  "DIV", "DATE", "HEAD", "TRLR", "NOTE"}
+months = { "JAN": 1, "FEB": 2, "MAR": 3, "APR": 4, "MAY": 5, "JUN": 6, "JUL": 7,
+"AUG": 8, "SEP": 9, "OCT": 10, "NOV": 11, "DEC": 12}
 if (len(sys.argv)!=2 ):
     sys.exit("Usage: gedcom_data.py <filename>")
 my_file = Path(sys.argv[1])
@@ -55,7 +59,6 @@ for line in Lines:
     #Check for individual table info
     if (tag=="INDI"):
         idi.append(id)
-        Age.append(0)
         Alive.append("True")
         Death.append("N/A")
         Child.append("N/A")
@@ -69,6 +72,12 @@ for line in Lines:
     if (tag=="DATE"):
         if (birth):
             Birthday.append(line.strip()[length:])
+            date=Birthday[len(Birthday)-1].split()
+            day=int(date[0])
+            month=months[date[1]]
+            year= int(date[2])
+            today = datetime.datetime.now()
+            Age.append(today.year - year - ((today.month, today.day) < (month, day)))
             birth=0
         if (death):
             Death[len(Alive)-1] = line.strip()[length:]
@@ -85,6 +94,10 @@ for line in Lines:
         idf.append(id)
         Married.append("N/A")
         Divorced.append("N/A")
+        Husband_ID.append("N/A")
+        Husband_Name.append("N/A")
+        Wife_ID.append("N/A")
+        Wife_Name.append("N/A")
         Children.append("N/A")
     if (marry):
         Married[len(Married)-1] = line.strip()[length:]
@@ -97,13 +110,74 @@ for line in Lines:
     if (tag=="DIV"):
         divorce=1
     if (tag=="HUSB"):
-        Husband_ID.append(line.strip()[length:])
-        Husband_Name.append(Name[idi.index(Husband_ID[len(Husband_ID)-1])])
+        Husband_ID[len(Husband_ID)-1] = line.strip()[length:]
+        Husband_Name[len(Husband_Name)-1] = Name[idi.index(Husband_ID[len(Husband_ID)-1])]
     if (tag=="WIFE"):
-        Wife_ID.append(line.strip()[length:])
-        Wife_Name.append(Name[idi.index(Wife_ID[len(Wife_ID)-1])])
+        Wife_ID[len(Wife_ID)-1] = line.strip()[length:]
+        Wife_Name[len(Wife_Name)-1] = Name[idi.index(Wife_ID[len(Wife_ID)-1])]
     if (tag=="CHIL"):
-        Children[len(Children)-1] = line.strip()[length:]
+        if (Children[len(Children)-1]=="N/A"):
+            Children[len(Children)-1] = line.strip()[length:]
+        else:
+            Children[len(Children)-1] += " {}".format(line.strip()[length:])
+
+
+#Check if marriage before death
+MarrbeforeBirth = []
+MarryAfterDeath = []
+DivorcedAfterDeath = []
+for i in range(len(idf)):
+    #date of marriage converted to usable format
+    marrydate=Married[i].split()
+    marryday=int(marrydate[0])
+    marrymonth=months[marrydate[1]]
+    marryyear=int(marrydate[2])
+    #Husband birthday converted
+    birthdate=Birthday[idi.index(Husband_ID[i])].split()
+    birthday=int(birthdate[0])
+    birthmonth=months[birthdate[1]]
+    birthyear= int(birthdate[2])
+    if (marryyear - birthyear - ((marrymonth, marryday) < (birthmonth, birthday)))<0:
+        MarrbeforeBirth.append(Husband_ID[i])
+    #Wife birthday converted
+    birthdate=Birthday[idi.index(Wife_ID[i])].split()
+    birthday=int(birthdate[0])
+    birthmonth=months[birthdate[1]]
+    birthyear= int(birthdate[2])
+    if (marryyear - birthyear - ((marrymonth, marryday) < (birthmonth, birthday)))<0:
+        MarrbeforeBirth.append(Wife_ID[i])
+    #Check if died
+    if(Alive[idi.index(Husband_ID[i])]=="False"):
+        deathdate=Death[idi.index(Husband_ID[i])].split()
+        deathday=int(deathdate[0])
+        deathmonth=months[deathdate[1]]
+        deathyear= int(deathdate[2])
+        if(Divorced[i]!="N/A"):
+            divorcedate=Divorced[i].split()
+            divorceday=int(divorcedate[0])
+            divorcemonth=divorcedate[divorcedate[1]]
+            divorceyear=int(divorcedate[2])
+            if (deathyear - divorceyear - ((deathmonth, deathday) < (divorcemonth, divorceday)))<0:
+                DivorcedAfterDeath.append(Husband_ID[i])
+        if (deathyear - marryyear - ((deathmonth, deathday) < (marrymonth, marryday)))<0:
+            MarryAfterDeath.append(Husband_ID[i])
+    if(Alive[idi.index(Wife_ID[i])]=="False"):
+        deathdate=Death[idi.index(Wife_ID[i])].split()
+        deathday=int(deathdate[0])
+        deathmonth=months[deathdate[1]]
+        deathyear= int(deathdate[2])
+        if(Divorced[i]!="N/A"):
+            divorcedate=Divorced[i].split()
+            divorceday=int(divorcedate[0])
+            divorcemonth=divorcedate[divorcedate[1]]
+            divorceyear=int(divorcedate[2])
+            if (deathyear - divorceyear - ((deathmonth, deathday) < (divorcemonth, divorceday)))<0:
+                DivorcedAfterDeath.append(Wife_ID[i])
+        if (deathyear - marryyear - ((deathmonth, deathday) < (marrymonth, marryday)))<0:
+            MarryAfterDeath.append(Wife_ID[i])
+         
+
+
 #Adding to Individuals Table
 Individuals.add_column("ID", idi)
 Individuals.add_column("Name", Name)
@@ -129,4 +203,5 @@ file1.write("Individuals\n")
 file1.write("{}\n".format(Individuals))
 file1.write("Families\n")
 file1.write("{}".format(Families))
+#file1.write("\n{}".format(MarryAfterDeath))
 file1.close()
